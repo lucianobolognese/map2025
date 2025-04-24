285627ca671244fdddcbcc17b8fd1bf01c5cc917
Luciano Bolognese <62243227+lucianobolognese@users.noreply.github.com>
2025-04-24 16:45:54 +0200

    Aggiornamenti qt1 e qt2 e inizio progetto

---
+++ QT/src/Attribute.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+abstract class Attribute {
+    /**
+     * Nome simbolico dell'attributo
+     */
+    private String name;
+    /**
+     * Identificativo numerico dell'attributo 
+     */
+    private int index;
+}
